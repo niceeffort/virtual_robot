1282169c92c56fe861c53f071079f0d7ca89db95
David Schoemehl <davidschoemehl@gmail.com>
2024-02-08 08:15:35 -0600

    Test files for breaking out work

--- TeamCode/src/org/firstinspires/ftc/teamcode/DavidTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/DavidTest.java
@@ -3,17 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.joebotics.HelloWorld;
+
 @TeleOp(name="David")
 public class DavidTest extends LinearOpMode {
+
+    HelloWorld myHelloWorld;
     @Override
     public void runOpMode() throws InterruptedException {
+        myHelloWorld = new HelloWorld(this);
+        double answer = Math.pow(2, 4);
+        answer = Math.sqrt(answer);
         telemetry.addLine("Hello World");
+        telemetry.addData("Answer", answer);
         telemetry.update();
+
         waitForStart();
         int counter = 0;
         while(opModeIsActive()){
-            counter++;
-            telemetry.addData("Counter",counter);
+            //counter++;
+            //telemetry.addData("Counter",counter);
+            myHelloWorld.Update();
             telemetry.update();
         }
     }
