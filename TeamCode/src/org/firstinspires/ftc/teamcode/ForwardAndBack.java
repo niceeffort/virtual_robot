ec1dc2b33db0393a9ce026eb3b600d14a66a5124
niceeffort <davidschoemehl@gmail.com>
2024-08-26 20:31:18 -0500

    Sample code for tutorial.

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ForwardAndBack.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Forward and Back", group = "Samples")
+public class ForwardAndBack extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        double motorPower = 0.0;
+
+        DcMotor m1 = hardwareMap.dcMotor.get("back_left_motor");
+        DcMotor m2 = hardwareMap.dcMotor.get("front_left_motor");
+        DcMotor m3 = hardwareMap.dcMotor.get("front_right_motor");
+        DcMotor m4 = hardwareMap.dcMotor.get("back_right_motor");
+
+        DcMotor[] motors = {m1,m2,m3,m4};
+
+        m1.setDirection(DcMotor.Direction.REVERSE);
+        m2.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addLine("Hello World!");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if(gamepad1.y) {
+                motorPower = 1.0;
+            } else if(gamepad1.a){
+                motorPower = -1.0;
+            } else {
+                motorPower = -gamepad1.left_stick_y;
+            }
+
+            for(DcMotor motor : motors){
+                motor.setPower(motorPower);
+            }
+
+            telemetry.addData("Motor Power", motorPower);
+            telemetry.update();
+        }
+    }
+}
